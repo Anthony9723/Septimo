932c46fbb1745dac8c44b4f09ba8e402ef3d6626
Anthony9723 <67307886+Anthony9723@users.noreply.github.com>
2020-07-06 16:49:10 -0500

    ClientesMigration

---
+++ ProyectoGit/Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoGit.Models
+{
+    public class Clientes
+    {
+        public int ClienteId { get; set; }
+        public String Cedula{ get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public String Direccion { get; set; }
+        public String Correo { get; set; }
+        public String Telefono { get; set; }
+    }
+}
